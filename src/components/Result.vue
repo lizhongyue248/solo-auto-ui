abe0445180e9503100cce83238c042278ceb9b36
lizhongyue248 <lizhongyue248@163.com>
2019-06-09 17:47:25 +0800

    :rocket: Deploying stuff.

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -6,6 +6,23 @@
       </md-card-header>
       <md-card-content>
       <pre>{{ JSON.stringify(config, null, 1) }}</pre>
+      <md-content class="md-primary">
+        前台运行，假设配置文件与 jar 包位于同一目录且名称为 solo-auto.json (点击命令复制)
+        <md-content class="md-accent"
+                    v-clipboard:copy="command1"
+                    v-clipboard:success="handleSuccess"
+                    v-clipboard:error="handleError">
+          {{command1}}
+        </md-content>
+        后台运行，假设配置文件与 jar 包位于同一目录且名称为 solo-auto.json (点击命令复制)
+        <md-content class="md-accent"
+                    v-clipboard:copy="command2"
+                    v-clipboard:success="handleSuccess"
+                    v-clipboard:error="handleError">
+          {{command2}}
+        </md-content>
+      </md-content>
+
       </md-card-content>
       <md-card-actions>
         <md-button class="md-raised md-primary"
@@ -30,6 +47,8 @@ export default class Result extends Vue {
   @Prop() private config!: Object
   message: String = ''
   showSnackbar: Boolean = false
+  command1: String = 'java -jar soloauto-1.0.0-SNAPSHOT-fat.jar -conf solo-auto.json'
+  command2: String = 'nohup java -jar soloauto-1.0.0-SNAPSHOT-fat.jar -conf solo-auto.json >/dev/null 2>&1 &'
 
   handleSuccess (): void {
     this.message = '复制成功'
@@ -50,5 +69,12 @@ pre {
   white-space:-moz-pre-wrap; /* Firefox */
   white-space:-o-pre-wrap; /* Opera 7 */
   word-wrap:break-word; /* Internet Explorer 5.5+ */
+}
+.md-content.md-primary{
+  padding: 1rem;
+}
+
+.md-content.md-accent{
+  padding: 0.5rem;
 }
 </style>
